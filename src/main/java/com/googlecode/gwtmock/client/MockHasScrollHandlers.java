20930b06c1ab0d8cb2b9258588c29663626f3da3
Daniel Kurka <kurka.daniel@googlemail.com>
2012-08-05 17:40:54 +0200

    fixing for gwt 2.5

--- src/main/java/com/googlecode/gwtmock/client/MockHasScrollHandlers.java
+++ src/main/java/com/googlecode/gwtmock/client/MockHasScrollHandlers.java
@@ -1,16 +1,14 @@
 /*
  * Copyright 2012 Katharina Fahnenbruck
  * 
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
  * 
  * http://www.apache.org/licenses/LICENSE-2.0
  * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
  * the License.
  */
 package com.googlecode.gwtmock.client;
@@ -26,20 +24,19 @@ import com.google.gwt.event.shared.SimpleEventBus;
  * @author Katharina Fahnenbruck
  * 
  */
-public class MockHasScrollHandlers implements HasScrollHandlers {
+public class MockHasScrollHandlers extends MockHasHandlers implements HasScrollHandlers {
 
-	protected EventBus eventBus = new SimpleEventBus();
+  protected EventBus eventBus = new SimpleEventBus();
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * com.google.gwt.event.dom.client.HasScrollHandlers#addScrollHandler(com
-	 * .google.gwt.event.dom.client.ScrollHandler)
-	 */
-	@Override
-	public HandlerRegistration addScrollHandler(ScrollHandler handler) {
-		return eventBus.addHandler(ScrollEvent.getType(), handler);
-	}
+  /*
+   * (non-Javadoc)
+   * 
+   * @see com.google.gwt.event.dom.client.HasScrollHandlers#addScrollHandler(com
+   * .google.gwt.event.dom.client.ScrollHandler)
+   */
+  @Override
+  public HandlerRegistration addScrollHandler(ScrollHandler handler) {
+    return eventBus.addHandler(ScrollEvent.getType(), handler);
+  }
 
 }
