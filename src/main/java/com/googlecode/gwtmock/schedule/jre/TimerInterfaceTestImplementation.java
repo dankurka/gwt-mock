787fc40fee0340477b791a3f9eded456fb13a833
Daniel Kurka <kurt@ncco.de>
2012-09-13 01:23:50 +0200

    added java timer impl

---
+++ src/main/java/com/googlecode/gwtmock/schedule/jre/TimerInterfaceTestImplementation.java
@@ -0,0 +1,70 @@
+package com.googlecode.gwtmock.schedule.jre;
+
+import java.util.LinkedList;
+
+import com.google.gwt.core.client.Scheduler.ScheduledCommand;
+import com.googlecode.gwtmock.schedule.client.TimerInterface;
+import com.googlecode.gwtmock.schedule.client.TimingRequest;
+
+public class TimerInterfaceTestImplementation implements TimerInterface{
+	private long now;
+	
+	private LinkedList<TimingEntry> entries = new LinkedList<TimerInterfaceTestImplementation.TimingEntry>();
+
+	
+	private class TimingEntry{
+		private ScheduledCommand command;
+		private long time;
+		
+		public TimingEntry(ScheduledCommand command, long time) {
+			this.command = command;
+			this.time = time;
+		}
+		
+		public long getTime() {
+			return time;
+		}
+		
+		public ScheduledCommand getCommand() {
+			return command;
+		}
+
+	
+	}
+	
+	@Override
+	public TimingRequest schedule(ScheduledCommand command, int time) {
+		final TimingEntry entry = new TimingEntry(command, now + time);
+		
+		entries.add(entry);
+		
+		
+		return new TimingRequest() {
+			
+			@Override
+			public void cancel() {
+				entries.remove(entry);
+				
+			}
+		};
+	}
+
+	@Override
+	public TimingRequest scheduleRepeatin(ScheduledCommand command, int time) {
+		throw new RuntimeException("not implemented");
+	}
+	
+	
+	public void setNow(long now) {
+		this.now = now;
+		
+		for (TimingEntry entry : entries) {
+			if(entry.getTime() <= now){
+				entry.getCommand().execute();
+			}
+		}
+		
+		
+	}
+	
+}
